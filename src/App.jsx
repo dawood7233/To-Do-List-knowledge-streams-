45f22b5cc63062f097916d6fa2833e1144a202a7
dawood <m.dawood7233@gmail.com>
2023-11-16 10:53:07 +0500

    to-do-list

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import TodoList from "./assets/components/To-Do-Component"
+
+ const App = ()=>{
+  return(
+    
+    <div>
+      <TodoList></TodoList>
+
+    </div>
+  )
+ }
+
+
+
+export default App
